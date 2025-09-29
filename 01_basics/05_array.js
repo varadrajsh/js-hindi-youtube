5495f5b700797743bc21a1ea968199be34f3d308
varadrajsh <varadrajsh@gmail.com>
2025-09-29 22:58:11 +0530

    array

--- 01_basics/05_array.js
+++ 01_basics/05_array.js
@@ -1,10 +1,5 @@
 const marvel_heroes = ["thor", "ironman", "spiderman"];
 const dc_heroes = ["superman", "flash", "barman"];
-console.log(marvel_heroes);
-console.log(dc_heroes);
-
-marvel_heroes.push(dc_heroes);
-console.log(marvel_heroes);
 // console.log(marvel_heroes);
 // console.log(dc_heroes);
 
@@ -14,5 +9,16 @@ console.log(marvel_heroes);
 // Difference o=in push and concat
 
 //marvel_heroes.concat(dc_heroes); 
-const all_heroes = marvel_heroes.concat(dc_heroes); // For concat we have to store it in variable
+//const all_heroes = marvel_heroes.concat(dc_heroes); // For concat we have to store it in variable
 //console.log(all_heroes);
+
+// using spread operators
+// const all_heroes_new = [...marvel_heroes, ...dc_heroes];
+// console.log(all_heroes_new);
+
+
+// Usinf Flat Array
+const another_array = [1,2,3,[4,5],[6,[7,8]],9];
+console.log(another_array);
+const flat_array = another_array.flat(Infinity);
+console.log(flat_array);
